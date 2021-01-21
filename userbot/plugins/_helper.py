a80aef1bf6f6c9547b5387aac4ab639c1c2b976f
Programming Error <75001577+ProgrammingError@users.noreply.github.com>
2021-01-21 07:30:27 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -1,17 +1,15 @@
-from userbot import CMD_LIST
-from userbot import ALIVE_NAME
+from telethon import functions
+
+from userbot import ALIVE_NAME, CMD_LIST
 from userbot.utils import admin_cmd, sudo_cmd
-from platform import uname
-import sys
-from telethon import events, functions, __version__
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "@Dark_cobra_support_group"
 
-#@command(pattern="^.help ?(.*)")
+# @command(pattern="^.help ?(.*)")
 @borg.on(admin_cmd(pattern=r"help ?(.*)", outgoing=True))
 @borg.on(sudo_cmd(pattern=r"help ?(.*)", outgoing=True, allow_sudo=True))
 async def cmd_list(event):
-    if not event.text[0].isalpha() and event.text[0] not in ("/" , "#", "-", "_", "@"):
+    if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "-", "_", "@"):
         tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
         input_str = event.pattern_match.group(1)
         if tgbotusername is None or input_str == "text":
@@ -31,7 +29,7 @@ async def cmd_list(event):
                         force_document=True,
                         allow_cache=False,
                         caption="**COMMANDS** In DARK COBRA",
-                        reply_to=reply_to_id
+                        reply_to=reply_to_id,
                     )
                     await event.delete()
             else:
@@ -46,19 +44,16 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = f"""Userbot Helper.. Provided by ✨{DEFAULTUSER}✨ \n
-`Userbot Helper to reveal all the commands`\n__Do .help plugin_name for commands, in case popup doesn't appear.__"""
+            help_string = "help"
             results = await bot.inline_query(  # pylint:disable=E0602
-                tgbotusername,
-                help_string
+                tgbotusername, help_string
             )
             await results[0].click(
-                event.chat_id,
-                reply_to=event.reply_to_msg_id,
-                hide_via=True
+                event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
             )
             await event.delete()
-            
+
+
 @borg.on(admin_cmd(pattern="dc"))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
@@ -86,7 +81,7 @@ async def _(event):
     if plugin_name in CMD_LIST:
         help_string = CMD_LIST[plugin_name].__doc__
         unload_string = f"Use `.unload {plugin_name}` to remove this plugin.\n           © Dark Cobra"
-        
+
         if help_string:
             plugin_syntax = f"Syntax for plugin **{plugin_name}**:\n\n{help_string}\n{unload_string}"
         else:
