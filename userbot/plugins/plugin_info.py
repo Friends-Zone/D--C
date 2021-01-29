fd6598a7f1b42c368ed1b0c1dc3c0c1142120285
Programming Error <75001577+ProgrammingError@users.noreply.github.com>
2021-01-29 09:31:04 +0530

    Update plugin_info.py

--- userbot/plugins/plugin_info.py
+++ userbot/plugins/plugin_info.py
@@ -6,27 +6,26 @@
 """ Userbot plugin_info command """
 
 from userbot import CMD_HELP
-from userbot.utils import admin_cmd
+from userbot.utils import admin_cmd, sudo_cmd, CmdHelp, edit_or_reply
 
 
 @borg.on(admin_cmd(outgoing=True, pattern="plinfo(?: |$)(.*)"))
+@borg.on(sudo_cmd(allow_sudo=True, pattern="plinfo(?: |$)(.*)"))
 async def info(event):
     """ For .plinfo command,"""
     args = event.pattern_match.group(1).lower()
     if args:
-        if args in CMD_HELP:
-            await event.edit(str(CMD_HELP[args]))
+        if args in CmdHelp:
+            await edit_or_reply(event, str(CmdHelp[args]))
         else:
-            await event.edit(
-                "Maybe the command help or plugin info has not been set or the plugin is invalid..."
+            await edit_or_reply(event,  "Maybe the command help or plugin info has not been set or the plugin is invalid..."
             )
     else:
-        await event.edit(
-            "Please specify which plugin do you want help for !!\
+        await edit_or_reply(event,  "Please specify which plugin do you want help for !!\
             \nUsage: .pinfo <plugin name>"
         )
         string = ""
-        for i in CMD_HELP:
+        for i in CmdHelp:
             string += "`" + str(i)
             string += "`\n"
         await event.reply(string)
