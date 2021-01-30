b7e91d10c996bcd3ae9c3583cc17a3f3ade854c6
Programming Error <75001577+ProgrammingError@users.noreply.github.com>
2021-01-30 19:53:55 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -61,7 +61,8 @@ else:
     print("Assitant is Not Loading As U Have Disabled")
 
 try:
-    bot.tgbot.send_message(bot.uid, "DARK COBRA userbot AND YOUR ASSISTANT is Active Enjoy Join @DarkCobra_Support For Updates.")
+    async ok(eve):
+        bot.tgbot.send_message(bot.uid, "DARK COBRA userbot AND YOUR ASSISTANT is Active Enjoy Join @DarkCobra_Support For Updates.")
 except Exception as e:
     print(e)
 print("DARK COBRA userbot AND YOUR ASSISTANT is Active Enjoy Join @DarkCobra_Support For Updates.")
